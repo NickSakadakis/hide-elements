34cf69ec70525ace857b6a6e6635a679407205f6
Nick Sakadakis <57203165+NickSakadakis@users.noreply.github.com>
2023-11-25 11:19:14 +0200

    Update span_hidden.js

--- span_hidden.js
+++ span_hidden.js
@@ -1,1 +1,1 @@
-setTimeout(() => {  document.querySelector('[id^=jizsl_]').setAttribute("style","visibility: hidden;"); }, 10000);
+document.querySelector('[id^=jizsl_]').setAttribute("style","visibility: hidden;");
